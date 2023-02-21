52047b44f93e298a81a01a506b961a1df8d2a5f2
g_rabbany <golamrabbany10@gmailcom>
2023-02-21 20:07:55 +0600

    category delete added

--- app/Http/Livewire/Admin/AdminCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminCategoryComponent.php
@@ -9,6 +9,13 @@ use Livewire\WithPagination;
 class AdminCategoryComponent extends Component
 {
     use WithPagination;
+
+    public function deleteCategory($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        session()->flash('message', 'Category has been Deleted Successfully');
+    }
     public function render()
     {
         $categories = Category::paginate(5);
